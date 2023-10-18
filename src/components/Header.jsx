e7d46b0928b4ad32d3fc9a11a74bc8328dad489b
AJ1732 <ejemeniboi@gmail.com>
2023-10-18 19:06:03 +0100

    Working on Cart functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiShoppingCart } from 'react-icons/fi';
-import Categories from '../pages/Categories';
 
 const Header = () => {
+  const [ isOpen , setIsOpen] = useState(false);
+  console.log(isOpen);
+
   return (
     <header className='relative px-4 flex flex-col justify-between'>
       <h1 className='font-poppins text-4xl font-extrabold m-auto py-10'>Rsa Stores</h1>
 
-      <div className='fixed right-0 top-9 | bg-ft-grey w-12 h-12 | flex justify-center items-center | text-white text-center text-2xl font-extrabold rounded-l-sm | '>
+      <button 
+        onClick={ () => setIsOpen(!isOpen) } 
+        className='fixed right-0 top-9 | bg-ft-grey w-12 h-12 | flex justify-center items-center | text-white text-center text-2xl font-extrabold rounded-l-sm | '
+      >
         <FiShoppingCart />
-      </div>
+      </button>
     </header>
   )
 }
